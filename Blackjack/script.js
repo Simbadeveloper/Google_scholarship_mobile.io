2ee06d47b893aa6f92e1899f8742c72a22cd5dcc
silas <silverdeltamega@gmail.com>
2018-11-29 19:08:58 +0300

    start blackjack

---
+++ Blackjack/script.js
@@ -0,0 +1,13 @@
+//
+//Blackjack
+//by silas
+//
+
+let card1 ="Ace of spades",
+    card2 ="Ten of Hearts";
+
+console.log("Welcome to Blacckjack")
+
+console.log("You are dealt:  ");
+console.log("  " + card1);
+console.log("  " + card2);
