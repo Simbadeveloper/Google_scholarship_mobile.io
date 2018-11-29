0c0fefed309939353dab763dbebec4bb13775812
silas <silverdeltamega@gmail.com>
2018-11-29 18:54:56 +0300

    string and number

--- script.js
+++ script.js
@@ -11,3 +11,45 @@ let productName = "Hammer";
       productId =  "H123";
 
 console.log(productName,productId);
+
+/*string */
+let productName = 'John's Hammer';
+
+console.log(productName);
+/*get an error causing using singlr quote*/
+
+/*string */
+let productName = 'John'\s Hammer';
+
+console.log(productName);
+/*use backslash*/
+
+/*Numbers*/
+let price = 5.00,
+    price2 = 3.00,
+    price3 = 4.00;
+
+console.log(price,price2);
+console.log(price+price2);
+console.log(price+price3);
+
+
+let firstName = "silas",
+    lastName = "Omurunga";
+
+console.log(firstName + lastName);
+console.log(firstName + " " + lastName);
+
+/*simple operation*/
+let first = 12,
+    second = 13;
+
+let answer = first + second;
+
+console.log(first,second);
+console.log(answer);
+
+//comment
+/*
+    large cooment
+*/
