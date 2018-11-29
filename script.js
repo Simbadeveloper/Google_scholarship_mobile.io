f7f402f5edb8ce26f50b8cf315a2417e1651ed1f
silas <silverdeltamega@gmail.com>
2018-11-29 18:25:29 +0300

    variable

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Hello world");
+
+/* variable */
+let productName = "Hammer";
+let productId =  "H123";
+
+console.log(productName,productId);
+
+/* or*/
+let productName = "Hammer";
+      productId =  "H123";
+
+console.log(productName,productId);
